81799c5cf1e2811022b61938ad4a9fd14525f249
Daniel <rodriguezdl@vcu.edu>
2019-06-07 18:23:13 -0400

    Created new media_player that is more up to date. - got music to work (now need to figure out how to get ext./int. read permissions so that songs from internal media storage can be displayed.

---
+++ app/src/main/java/com/example/daniel/music_player_2/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.daniel.music_player_2;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button playButton;
+    private SeekBar positionBar, volBar;
+    private TextView elapsedTimeLabel, remainingTimeLabel;
+    private MediaPlayer mediaPlayer;
+    private int totalTime;
+    private Object View;
+    private Object view;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playButton = findViewById(R.id.play_button);
+        elapsedTimeLabel = findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = findViewById(R.id.remainingTimeLabel);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.patiencebeat);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.setVolume(0.5f, 0.5f);
+        totalTime = mediaPlayer.getDuration();
+
+        positionBar = (SeekBar) findViewById(R.id.positionBar);
+        positionBar.setMax(totalTime);
+        positionBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                    positionBar.setProgress(progress);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+        });
+
+        volBar = (SeekBar) findViewById(R.id.volumeBar);
+        volBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float volumeNumber = progress / 100f;
+                mediaPlayer.setVolume(volumeNumber, volumeNumber);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mediaPlayer != null) {
+                    try {
+                        Message message = new Message();
+                        message.what = mediaPlayer.getCurrentPosition();
+                        handler.sendMessage(message);
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        });
+    }
+
+    private Handler handler = new Handler() {
+        public void handleMessge(Message message) {
+            int currentPosition = message.what;
+            positionBar.setProgress(currentPosition);
+
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+            String remainingTime = createTimeLabel(totalTime - currentPosition);
+            remainingTimeLabel.setText("- " + remainingTime);
+
+
+        }
+    };
+
+    private String createTimeLabel(int time) {
+        String timeLabel = "";
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if (sec < 10) {
+            timeLabel += "0";
+            timeLabel += sec;
+        }
+        return timeLabel;
+    }
+
+        public void playBtnClick(android.view.View view){
+            if (!mediaPlayer.isPlaying()) {
+                mediaPlayer.start();
+                //playButton.setBackgroundResource(R.drawable.sound);
+            } else {
+                mediaPlayer.pause();
+            }
+        }
+}
+
+
